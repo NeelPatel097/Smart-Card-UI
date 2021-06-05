bee543dc7445651640ee06f89d101d9ac9b3a1a2
Neel Patel <np7.patel.np@gmail.com>
2021-06-05 20:35:16 +0530

    Second Commit

--- lib/Views/SmartCardPage.dart
+++ lib/Views/SmartCardPage.dart
@@ -1,4 +1,5 @@
 import 'package:farmer_smart_card_ui/widgets/card_front.dart';
+import 'package:farmer_smart_card_ui/widgets/card_back.dart';
 import 'package:flutter/material.dart';
 
 class SmartCardPage extends StatefulWidget {
@@ -7,6 +8,9 @@ class SmartCardPage extends StatefulWidget {
 }
 
 class _SmartCardPageState extends State<SmartCardPage> {
+
+  double _rotationFactor = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +20,19 @@ class _SmartCardPageState extends State<SmartCardPage> {
       body: Center(
         child: Column(
           children: <Widget>[
-            CardFrontView(),
+            Transform(
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001)
+                  ..rotateY(3.14 * _rotationFactor),
+                origin: Offset(MediaQuery.of(context).size.width / 2, 0),
+                child: _rotationFactor < 0.5 ? CardFrontView() : CardBackView()),
+            Slider(
+              onChanged: (double value) {
+                setState(() {
+                  _rotationFactor = value;
+                });
+              },
+              value: _rotationFactor,)
           ],
         ),
       ),
--- lib/widgets/card_front.dart
+++ lib/widgets/card_front.dart
@@ -4,32 +4,59 @@ class CardFrontView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 500,
+      height: 300,
       child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 8,
         margin: EdgeInsets.all(16),
-        child: Column(
-          children: <Widget>[
-            Image.asset('assets/visa.png'),
-            Text('0000 1000 9012 3456'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Farmer Name'),
-                    Text('Neel Patel'),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Date of Birth'),
-                    Text('01/12/1972'),
-                  ],
-                ),
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Align(
+                  alignment: Alignment.topRight,
+                  child: Image.asset('assets/visa.png')),
+              SizedBox(
+                height: 32,
+              ),
+              Text('0000 1000 9012 3456',
+                  style: TextStyle(
+                      fontSize: 22,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.w500,
+                      shadows: [Shadow(color: Colors.black12, offset: Offset(2, 1))])),
+              SizedBox(
+                height: 32,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Farmer Name'),
+                      Text('Neel Patel',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Date of Birth'),
+                      Text('01/12/1972',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
